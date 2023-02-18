5819518e42764259b6e577680246d6eace8fdd83
Brandon Tai <btai@bayviewglen.ca>
2023-02-18 18:52:49 -0500

    route meta fields next

--- Travel App Options/src/App.vue
+++ Travel App Options/src/App.vue
@@ -5,8 +5,11 @@
       <br>
       
   <div class="container">
-
-    <router-view :key="$route.path"></router-view>
+    <router-view v-slot="{Component}">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" :key="$route.path"></component>
+    </transition>
+  </router-view>
 
   </div>
 </template>
@@ -18,3 +21,11 @@ export default{
 }
 </script>
 
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
--- Travel App Options/src/router/index.js
+++ Travel App Options/src/router/index.js
@@ -1,14 +1,39 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
-
+import sourceData from '../data.json'
 const router = createRouter({
     history: createWebHistory(), 
     routes:[
         {path: '/', name: 'Home', component: Home},
        
-        {path: '/destination/:id/:slug', name:'destination.show', component: ()=>import('../views/DestinationShow.vue')}
-    ]
-
+        {path: '/destination/:id/:slug', 
+        name:'destination.show', 
+        component: ()=>import('../views/DestinationShow.vue'),
+        props: route=> ({...route.params,id: parseInt(route.params.id)}),
+        beforeEnter(to, from){
+            const exists = sourceData.destinations.find(destination => destination.id === parseInt(to.params.id))
+            if(!exists){
+                return {name: 'NotFound', params: { pathMatch: to.path.substring(1).split('/')}, query: to.query, hash: to.hash}
+            }
+        },
+        children:[
+            {path:':experienceSlug',
+            name:'experience.show',
+            component: ()=>import('../views/ExperienceShow.vue'),
+            props: route=> ({...route.params,id: parseInt(route.params.id)})
+        
+        }
+        ]
+    }, 
+    {path: '/:pathMatch(.*)*', name: 'NotFound', component: ()=>import('../views/NotFound.vue')}
+    
+    ],
+    scrollBehavior(to, from, savedPosition){
+        return savedPosition || new Promise((resolve)=>{
+            setTimeout(()=> resolve({top:0, behavior:'smooth'}), 300)
+        })
+        return {top:null, left: null, behavior: 'null'}
+    }
 });
 
 export default router;
